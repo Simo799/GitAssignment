b850b9101356700a91f2a994d38ccf79a759dc8a
Simona <Simona@DESKTOP-IM14VVS>
2020-03-20 11:02:58 +0200

    Al treilea commit:Am implementat o interfata si am testat functionalitatea clasei Angajat in metoda TestGit

--- Aplicatie penntru managementul proiectelor in cadrul unei companii/src/ro/ase/csie/clase/AngajatCompanie.java
+++ Aplicatie penntru managementul proiectelor in cadrul unei companii/src/ro/ase/csie/clase/AngajatCompanie.java
@@ -1,5 +1,54 @@
 package ro.ase.csie.clase;
 
-public class AngajatCompanie {
+public class AngajatCompanie implements InterfataGestionareAngajat{
+	 
+	private String numeAngajat;
+	private int salariuAngajat;
+	private String taskDorit;
+	private static int valoareMarire=200;
+	public String getNumeAngajat() {
+		return numeAngajat;
+	}
 
+	public void setNumeAngajat(String numeAngajat) {
+		this.numeAngajat = numeAngajat;
+	}
+
+	public int getSalariuAngajat() {
+		return salariuAngajat;
+	}
+
+	public void setSalariuAngajat(int salariuAngajat) {
+		this.salariuAngajat = salariuAngajat;
+	}
+
+	public String getTaskDorit() {
+		return taskDorit;
+	}
+	public void setTaskDorit(String taskDorit) {
+		this.taskDorit = taskDorit;
+	}
+
+	public AngajatCompanie(String numeAngajat, int salariuAngajat, String taskDorit) {
+		super();
+		this.numeAngajat = numeAngajat;
+		this.salariuAngajat = salariuAngajat;
+		this.taskDorit = taskDorit;
+	}
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Numele angajatului este ");
+		sb.append(this.numeAngajat);
+		sb.append(" si are salariul ");
+		sb.append(this.salariuAngajat);
+		sb.append(", acesta dorind sa primeasca ca task: ");
+		sb.append(this.taskDorit);
+		return sb.toString();
+	}
+
+	@Override
+	public double marireSalariu() {
+		return this.salariuAngajat+this.valoareMarire;
+	}
 }
--- Aplicatie penntru managementul proiectelor in cadrul unei companii/src/ro/ase/csie/clase/TestGit.java
+++ Aplicatie penntru managementul proiectelor in cadrul unei companii/src/ro/ase/csie/clase/TestGit.java
@@ -5,6 +5,10 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 
+		AngajatCompanie angajat=new AngajatCompanie("Ionel Rusu", 2000, "raport statistic");
+		System.out.println(angajat);
+		//angajat.marireSalariu();
+		System.out.println("Angajatul o sa aiba urmatorul salariu dupa marire "+angajat.marireSalariu());
 	}
 
 }
